003d1a6b7757f392fcd5bce0978be2f3d917eb42
Star3oy <115896405+Star3oy@users.noreply.github.com>
2024-05-10 19:28:07 -0600

    Cree tantas cosas que no sé como describir tanto

--- CycleCare/app/src/main/java/com/ikariscraft/cyclecare/activities/PrincipalScreen.java
+++ CycleCare/app/src/main/java/com/ikariscraft/cyclecare/activities/PrincipalScreen.java
@@ -5,12 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.ikariscraft.cyclecare.R;
+import com.ikariscraft.cyclecare.databinding.ActivityPrincipalScreenBinding;
 
 public class PrincipalScreen extends AppCompatActivity {
+
+    ActivityPrincipalScreenBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_principal_screen);
+        binding = ActivityPrincipalScreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(binding.fragmentContainer.getId(), new ViewSleepChart())
+                .addToBackStack(null)
+                .commit();
+
     }
 }
