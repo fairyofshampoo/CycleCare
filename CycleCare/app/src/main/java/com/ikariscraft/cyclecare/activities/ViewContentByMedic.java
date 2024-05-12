04f2b550ddf29cb1407f7889319c5195eb6e268b
Star3oy <115896405+Star3oy@users.noreply.github.com>
2024-05-11 18:57:13 -0600

    Terminé todas mis pantallas

---
+++ CycleCare/app/src/main/java/com/ikariscraft/cyclecare/activities/ViewContentByMedic.java
@@ -0,0 +1,16 @@
+package com.ikariscraft.cyclecare.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ikariscraft.cyclecare.R;
+
+public class ViewContentByMedic extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_content_by_medic);
+    }
+}
