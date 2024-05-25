ac8b5c0ad14f3e33edbd66d9f885ffe113d7c21b
ᓚᘏᗢ michelle ! <96703919+fairyofshampoo@users.noreply.github.com>
2024-05-25 09:35:54 -0600

    feat (cycle log) calendar ui

---
+++ CycleCare/app/src/main/java/com/ikariscraft/cyclecare/activities/cycle_log/CalendarFragment.java
@@ -0,0 +1,16 @@
+package com.ikariscraft.cyclecare.activities.cycle_log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ikariscraft.cyclecare.R;
+
+public class CalendarFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_calendar);
+    }
+}
