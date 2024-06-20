6727dc9bb9c2808a06fb71a68f7fc19b7f37f07d
ᓚᘏᗢ michelle ! <96703919+fairyofshampoo@users.noreply.github.com>
2024-06-20 17:52:26 -0600

    feat (cycle-log) create cycle log

--- CycleCare/app/src/main/java/com/ikariscraft/cyclecare/activities/cycle_log/UpdateCycleLogActivity.java
+++ CycleCare/app/src/main/java/com/ikariscraft/cyclecare/activities/cycle_log/UpdateCycleLogActivity.java
@@ -2,25 +2,12 @@ package com.ikariscraft.cyclecare.activities.cycle_log;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import com.ikariscraft.cyclecare.R;
 
 public class UpdateCycleLogActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_update_cycle_log);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 }
